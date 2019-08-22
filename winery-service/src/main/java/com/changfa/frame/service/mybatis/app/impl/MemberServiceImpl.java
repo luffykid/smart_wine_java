bef84ad6e1af4daaffb9b1917ea12d44fcc0f98c
wangyueyun <wangyueyun0414@126.com>
2019-08-22 19:44:08 +0800

    Java WYY：更新会员M

--- winery-service/src/main/java/com/changfa/frame/service/mybatis/app/impl/MemberServiceImpl.java
+++ winery-service/src/main/java/com/changfa/frame/service/mybatis/app/impl/MemberServiceImpl.java
@@ -8,7 +8,6 @@ import com.changfa.frame.service.mybatis.app.MemberService;
 import com.changfa.frame.service.mybatis.common.impl.BaseServiceImpl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.sun.org.apache.xerces.internal.xs.datatypes.ObjectList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +24,7 @@ import java.util.Map;
  * @date 2019-08-15 14:40
  */
 @Service("memberServiceImpl")
-public class MemberServiceImpl extends BaseServiceImpl<Member,Long> implements MemberService{
+public class MemberServiceImpl extends BaseServiceImpl<Member, Long> implements MemberService {
 
     @Autowired
     private MemberMapper memberMapper;
@@ -34,6 +33,7 @@ public class MemberServiceImpl extends BaseServiceImpl<Member,Long> implements M
 
     /**
      * 根据手机号查询会员
+     *
      * @param phone 手机号
      * @return
      */
@@ -44,6 +44,7 @@ public class MemberServiceImpl extends BaseServiceImpl<Member,Long> implements M
 
     /**
      * 获取招募会员列表
+     *
      * @param mbrId
      * @param pageInfo
      * @return
@@ -58,6 +59,7 @@ public class MemberServiceImpl extends BaseServiceImpl<Member,Long> implements M
 
     /**
      * 获取招募会员数据统计
+     *
      * @param mbrId
      * @return
      */
@@ -68,12 +70,13 @@ public class MemberServiceImpl extends BaseServiceImpl<Member,Long> implements M
 
     /**
      * 修改会员信息
+     *
      * @param mbrId
      * @param userIcon 头像
      * @param nickName 名称
      * @param birthday 生日
-     * @param sex 性别
-     * @param phone 电话
+     * @param sex      性别
+     * @param phone    电话
      */
     @Transactional
     @Override
@@ -83,13 +86,13 @@ public class MemberServiceImpl extends BaseServiceImpl<Member,Long> implements M
         member.setPhone(phone);
         memberMapper.update(member);
         List<MbrWechat> mbrWechats = mbrWechatMapper.selectListByMbrIdAndWineryId(member.getId(), member.getWineryId());
-        if (mbrWechats.size() == 0){
+        if (mbrWechats.size() == 0) {
             MbrWechat mbrWechat = new MbrWechat();
             mbrWechat.setNickName(nickName);
             mbrWechat.setBirthday(new Date(birthday));
             mbrWechat.setSex(sex);
             mbrWechatMapper.save(mbrWechat);
-        }else{
+        } else {
             MbrWechat mbrWechat = mbrWechats.get(0);
             mbrWechat.setNickName(nickName);
             mbrWechat.setBirthday(new Date(birthday));
