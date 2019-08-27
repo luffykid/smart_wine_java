eb4e33d343113afe9bd857b84067652c73d4ca64
lsj089 <584072089@qq.com>
2019-08-27 09:59:04 +0800

    修改mybatis xml文件

---
+++ winery-mapper/src/main/java/com/changfa/frame/mapper/app/WineryAdjustAdvanceMapper.java
@@ -0,0 +1,14 @@
+/*
+ * WineryAdjustAdvanceMapper.java
+ * Copyright(C) 北京畅发科技有限公司
+ * All rights reserved.
+ * -----------------------------------------------
+ * 2019-08-27 Created
+ */
+package com.changfa.frame.mapper.app;
+
+import com.changfa.frame.mapper.common.BaseMapper;
+import com.changfa.frame.model.app.WineryAdjustAdvance;
+
+public interface WineryAdjustAdvanceMapper extends BaseMapper<WineryAdjustAdvance, Long> {
+}
