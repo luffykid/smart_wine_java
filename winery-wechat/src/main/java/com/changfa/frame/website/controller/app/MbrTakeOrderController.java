0bc39c63ea2b46b22102d903268ae8fed966c59b
wangyueyun <wangyueyun0414@126.com>
2019-08-28 21:26:22 +0800

    Java WYY:提交取酒订单

--- winery-wechat/src/main/java/com/changfa/frame/website/controller/app/MbrTakeOrderController.java
+++ winery-wechat/src/main/java/com/changfa/frame/website/controller/app/MbrTakeOrderController.java
@@ -1,11 +1,11 @@
 package com.changfa.frame.website.controller.app;
 
+import com.changfa.frame.core.util.DateUtil;
 import com.changfa.frame.model.app.MbrStoreOrderItem;
 import com.changfa.frame.model.app.MbrTakeOrder;
 import com.changfa.frame.model.app.Member;
 import com.changfa.frame.service.mybatis.app.MbrStoreOrderService;
 import com.changfa.frame.service.mybatis.app.MbrTakeOrderService;
-import com.changfa.frame.service.mybatis.app.impl.MbrTakeOrderServiceImpl;
 import com.changfa.frame.website.controller.common.BaseController;
 import com.changfa.frame.website.controller.common.CustomException;
 import com.changfa.frame.website.controller.common.RESPONSE_CODE_ENUM;
@@ -14,7 +14,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,13 +23,10 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 会员商品订单接口
- *
  */
 @Api(value = "会员取酒订单接口", tags = "会员取酒订单接口")
 @RestController("wxMiniMbrTakeOrderController")
@@ -39,6 +37,7 @@ public class MbrTakeOrderController extends BaseController {
     private MbrTakeOrderService mbrTakeOrderServiceImpl;
     @Resource
     private MbrStoreOrderService mbrStoreOrderServiceImpl;
+
     /**
      * 获取荣誉庄主列表
      *
@@ -50,9 +49,34 @@ public class MbrTakeOrderController extends BaseController {
             @ApiImplicitParam(name = "mbrStoreOrderId", value = "会员储酒订单ID", dataType = "Long"))
     @RequestMapping(value = "/getList", method = RequestMethod.GET)
     public Map<String, Object> getList(Long mbrStoreOrderId, PageInfo pageInfo) {
-        PageInfo<MbrTakeOrder> list = mbrTakeOrderServiceImpl.getListByMbrStoreOrderId(mbrStoreOrderId, pageInfo);
+        // 查询数据集合
+        Map<String, List<MbrTakeOrder>> returnMap = new LinkedHashMap<>();
+        PageInfo<MbrTakeOrder> orderPageInfo = mbrTakeOrderServiceImpl.getListByMbrStoreOrderId(mbrStoreOrderId, pageInfo);
+        List<MbrTakeOrder> takeOrders = orderPageInfo.getList();
+        if (CollectionUtils.isEmpty(takeOrders)) {
+            return getResult(null);
+        }
 
-        return getResult(list);
+        // 按照时间格式返回列表数据
+        for (MbrTakeOrder takeOrder : takeOrders) {
+            Date createDate = takeOrder.getCreateDate();
+            String dateStr = DateUtil.convertDateToStr(createDate, "yyyy年MM月dd日");
+            returnMap.put(dateStr, new ArrayList<MbrTakeOrder>());
+        }
+        for (String dateStrKey : returnMap.keySet()) {
+            ArrayList<MbrTakeOrder> mbrTakeOrders = new ArrayList<>();
+            for (MbrTakeOrder takeOrder : takeOrders) {
+                Date createDate = takeOrder.getCreateDate();
+                String dateStr = DateUtil.convertDateToStr(createDate, "yyyy年MM月dd日");
+                if (StringUtils.equalsIgnoreCase(dateStr, dateStrKey)) {
+                    mbrTakeOrders.add(takeOrder);
+                }
+                continue;
+            }
+            returnMap.put(dateStrKey, mbrTakeOrders);
+        }
+
+        return getResult(returnMap);
     }
 
     /**
@@ -68,13 +92,13 @@ public class MbrTakeOrderController extends BaseController {
     public Map<String, Object> getDetail(Long id) {
 
         MbrTakeOrder mbrTakeOrder = mbrTakeOrderServiceImpl.getById(id);
-        if (mbrTakeOrder == null){
+        if (mbrTakeOrder == null) {
             throw new CustomException(RESPONSE_CODE_ENUM.NO_DATA);
         }
         List<MbrStoreOrderItem> list = mbrStoreOrderServiceImpl.getMbrStoreOrderItemByStoreId(mbrTakeOrder.getMbrStoreOrderId());
         Map<String, Object> returnMap = new HashMap<>();
-        returnMap.put("mbrTakeOrder",mbrTakeOrder);
-        returnMap.put("sku_name",list.get(0).getSkuName());
+        returnMap.put("mbrTakeOrder", mbrTakeOrder);
+        returnMap.put("sku_name", list.get(0).getSkuName());
         return getResult(returnMap);
     }
 
@@ -82,7 +106,7 @@ public class MbrTakeOrderController extends BaseController {
      * 自提
      *
      * @param mbrStoreOrderId 我的储酒订单Id
-     * @param takeWeight 取酒重量
+     * @param takeWeight      取酒重量
      * @return
      */
     @ApiOperation(value = "自提", notes = "自提")
@@ -94,9 +118,9 @@ public class MbrTakeOrderController extends BaseController {
     public Map<String, Object> takeInPerson(HttpServletRequest request, Long mbrStoreOrderId, BigDecimal takeWeight) {
         Member member = getCurMember(request);
         Long takeOrderId;
-        try{
+        try {
             takeOrderId = mbrTakeOrderServiceImpl.takeInPerson(mbrStoreOrderId, takeWeight);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.info("此处有错误:{}", "错误信息");
             throw new CustomException(RESPONSE_CODE_ENUM.UPDTATE_EXIST);
         }
