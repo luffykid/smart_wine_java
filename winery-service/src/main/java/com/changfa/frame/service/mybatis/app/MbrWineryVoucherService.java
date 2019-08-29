b5de56ccf930c5984668972acd53db791cdf1df1
lsj089 <584072089@qq.com>
2019-08-29 10:06:13 +0800

    添加部分接口

--- winery-service/src/main/java/com/changfa/frame/service/mybatis/app/MbrWineryVoucherService.java
+++ winery-service/src/main/java/com/changfa/frame/service/mybatis/app/MbrWineryVoucherService.java
@@ -3,6 +3,8 @@ package com.changfa.frame.service.mybatis.app;
 import com.changfa.frame.model.app.MbrWineryVoucher;
 import com.changfa.frame.service.mybatis.common.BaseService;
 
+import java.util.List;
+
 public interface MbrWineryVoucherService extends BaseService<MbrWineryVoucher, Long> {
 
     /**
@@ -11,4 +13,10 @@ public interface MbrWineryVoucherService extends BaseService<MbrWineryVoucher, L
      * @return
      */
     Integer getEnableVoucherCount(Long mbrId);
+
+    /**
+     * 获取可以使用的优惠券列表
+     * @return
+     */
+    List<MbrWineryVoucher> getEnableVoucherList(Long mbrId);
 }
