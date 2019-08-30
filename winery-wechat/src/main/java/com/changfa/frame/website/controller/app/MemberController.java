792b8725846873b259940650aa21a89fadc49861
wangyueyun <wangyueyun0414@126.com>
2019-08-30 21:41:37 +0800

    Java WYY:更新会员详情接口

--- winery-wechat/src/main/java/com/changfa/frame/website/controller/app/MemberController.java
+++ winery-wechat/src/main/java/com/changfa/frame/website/controller/app/MemberController.java
@@ -1,7 +1,6 @@
 package com.changfa.frame.website.controller.app;
 
 
-import com.changfa.frame.model.app.MbrWechat;
 import com.changfa.frame.model.app.Member;
 import com.changfa.frame.service.mybatis.app.MbrWechatService;
 import com.changfa.frame.service.mybatis.app.MbrWineryVoucherService;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
