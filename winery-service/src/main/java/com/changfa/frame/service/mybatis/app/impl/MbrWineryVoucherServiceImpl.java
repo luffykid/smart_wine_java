878059f38c3fdd3c8f73d2ea9f938ea2d3f9e06a
wangyueyun <wangyueyun0414@126.com>
2019-09-01 13:52:39 +0800

    Java WYY：删除优惠券列表

--- winery-service/src/main/java/com/changfa/frame/service/mybatis/app/impl/MbrWineryVoucherServiceImpl.java
+++ winery-service/src/main/java/com/changfa/frame/service/mybatis/app/impl/MbrWineryVoucherServiceImpl.java
@@ -7,6 +7,7 @@ import com.changfa.frame.service.mybatis.common.impl.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service("mbrWineryVoucherServiceImpl")
@@ -15,4 +16,14 @@ public class MbrWineryVoucherServiceImpl extends BaseServiceImpl<MbrWineryVouche
     @Autowired
     private MbrWineryVoucherMapper mbrWineryVoucherMapper;
 
+    /**
+     * 根据会员ID和时间查询有效优惠券
+     * @param mbrId
+     * @param nowDate
+     * @return
+     */
+    @Override
+    public List<MbrWineryVoucher> selectEffectByMbrId(Long mbrId, Date nowDate) {
+        return mbrWineryVoucherMapper.selectEffectByMbrId(mbrId,nowDate);
+    }
 }
