644817501fc947f369de66d5b721ad2055b2e87e
wangyueyun <wangyueyun0414@126.com>
2019-09-03 16:09:41 +0800

    Java WYY:更新数据

--- winery-wechat/src/main/java/com/changfa/frame/website/controller/app/MbrProdOrderController.java
+++ winery-wechat/src/main/java/com/changfa/frame/website/controller/app/MbrProdOrderController.java
@@ -1,10 +1,13 @@
 package com.changfa.frame.website.controller.app;
 
+import com.changfa.frame.core.weChat.WeChatPayUtil;
 import com.changfa.frame.model.app.MbrProdOrder;
 import com.changfa.frame.model.app.MbrProdOrderItem;
 import com.changfa.frame.model.app.Member;
 import com.changfa.frame.service.mybatis.app.MbrProdOrderService;
 import com.changfa.frame.website.controller.common.BaseController;
+import com.changfa.frame.website.controller.common.CustomException;
+import com.changfa.frame.website.controller.common.RESPONSE_CODE_ENUM;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -18,15 +21,14 @@ import java.util.Map;
 
 /**
  * 会员商品订单接口
- *
  */
 @Api(value = "会员商品订单接口", tags = "会员商品订单接口")
-@RestController("wxMbrProdOrderControllerr")
+@RestController("wxMiniMbrProdOrderController")
 @RequestMapping("/wxMini/auth/mbrProdOrder")
 public class MbrProdOrderController extends BaseController {
 
     @Resource(name = "mbrProdOrderServiceImpl")
-    private MbrProdOrderService mbrProdOrderServiceImpl;
+    private MbrProdOrderService mbrProdOrderService;
 
     /**
      * 获取我的订单分类信息
@@ -37,14 +39,14 @@ public class MbrProdOrderController extends BaseController {
     @ApiImplicitParams(
             @ApiImplicitParam(name = "status", value = "订单状态", dataType = "Integer"))
     @RequestMapping(value = "/getListByStatus", method = RequestMethod.GET)
-    public Map<String, Object> getListByTypes(Integer status ,HttpServletRequest request) {
+    public Map<String, Object> getListByTypes(Integer status, HttpServletRequest request) {
         Member member = getCurMember(request);
 
         List<MbrProdOrder> orderList = null;
-        if(status == null || "".equals(status)){
-            orderList = mbrProdOrderServiceImpl.getListByMbrId(member.getId());
-        }else{
-            orderList = mbrProdOrderServiceImpl.getListByStatus(member.getId(),status);
+        if (status == null || "".equals(status)) {
+            orderList = mbrProdOrderService.getListByMbrId(member.getId());
+        } else {
+            orderList = mbrProdOrderService.getListByStatus(member.getId(), status);
         }
 
         return getResult(orderList);
@@ -61,7 +63,7 @@ public class MbrProdOrderController extends BaseController {
 
         Member member = getCurMember(request);
         Long wineryId = getCurWineryId();
-        return getResult(mbrProdOrderServiceImpl.placeAnOrder(wineryId, member.getId(), items));
+        return getResult(mbrProdOrderService.placeAnOrder(wineryId, member.getId(), items));
 
     }
 
@@ -82,9 +84,9 @@ public class MbrProdOrderController extends BaseController {
 
         checkValidate(payMode);
 
-        MbrProdOrder order = mbrProdOrderServiceImpl.addMbrAddressInfoAndChoosePayMode(mbrProdOrderId,
-                                                                                        mbrAddressId,
-                                                                                        payMode);
+        MbrProdOrder order = mbrProdOrderService.addMbrAddressInfoAndChoosePayMode(mbrProdOrderId,
+                mbrAddressId,
+                payMode);
 
 
         Member member = getCurMember(request);
@@ -100,9 +102,34 @@ public class MbrProdOrderController extends BaseController {
         return getResult(order);
     }
 
+    /**
+     * 依据订单ID统一下单
+     */
+    @ApiOperation(value = "依据订单ID统一下单", notes = "依据订单ID统一下单")
+    @ApiImplicitParams({@ApiImplicitParam(name = "mbrProdOrderId", value = "会员商品订单ID", dataType = "Long"),})
+    @PostMapping("/unifiedOrder")
+    public Map<String, Object> unifiedOrderOfProdOrder(Long mbrProdOrderId, HttpServletRequest request) {
+        // 校验参数
+        if (mbrProdOrderId == null) {
+            throw new CustomException(RESPONSE_CODE_ENUM.MISS_PARAMETER);
+        }
+
+        // 查询商品订单信息
+        MbrProdOrder mbrProdOrder = mbrProdOrderService.getById(mbrProdOrderId);
+
+        // 微信小程序支付预下单
+        Map<String, Object> returnMap = WeChatPayUtil.unifiedOrderOfWxMini(mbrProdOrder.getOrderNo(), mbrProdOrder.getPayRealAmt(),
+                getCurMember(request).getOpenId(),
+                "/paymentNotify/async_notify/PRODUCT_ORDER.jhtml",
+                "酒庄酒订单",
+                request);
+
+        return getResult(returnMap);
+    }
+
     private void checkMemberIntegralMoreThanTheOrderShouldBePay(Member member, MbrProdOrder order) {
 
-        if(member.getTotalIntegral().compareTo(order.getPayIntegralCnt()) < 0)
+        if (member.getTotalIntegral().compareTo(order.getPayIntegralCnt()) < 0)
             throw new IllegalStateException("integral of the member don't enough!");
 
     }
@@ -110,8 +137,8 @@ public class MbrProdOrderController extends BaseController {
     private void checkValidate(Integer payMode) {
 
         if (payMode != MbrProdOrder.PAY_MODE_ENUM.WX_MINI_INTEGRAL_MODE.getValue()
-            && payMode != MbrProdOrder.PAY_MODE_ENUM.INTEGRAL_MODE.getValue()
-            && payMode != MbrProdOrder.PAY_MODE_ENUM.WX_MINI_MODE.getValue())
+                && payMode != MbrProdOrder.PAY_MODE_ENUM.INTEGRAL_MODE.getValue()
+                && payMode != MbrProdOrder.PAY_MODE_ENUM.WX_MINI_MODE.getValue())
             throw new IllegalArgumentException("the pay mode don't support!");
 
     }
