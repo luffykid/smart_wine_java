dd6f9ead81185d6d1e877261ed6db6c39c07fdde
= <=>
2019-09-03 18:38:55 +0800

    对同一sku的产品添加购物车之对其购物车项的产品增加相应的数量

--- winery-service/src/main/java/com/changfa/frame/service/mybatis/app/MbrCartService.java
+++ winery-service/src/main/java/com/changfa/frame/service/mybatis/app/MbrCartService.java
@@ -4,4 +4,13 @@ import com.changfa.frame.model.app.MbrCart;
 import com.changfa.frame.service.mybatis.common.BaseService;
 
 public interface MbrCartService extends BaseService<MbrCart, Long> {
+
+    /**
+     * 添加商品到购物车
+     * @param mbrId 会员id
+     * @param wineryId 酒庄id
+     * @param cartAdding
+     * @return
+     */
+    MbrCart addProd(Long mbrId, Long wineryId, MbrCart cartAdding);
 }
--- winery-service/src/main/java/com/changfa/frame/service/mybatis/app/impl/MbrCartServiceImpl.java
+++ winery-service/src/main/java/com/changfa/frame/service/mybatis/app/impl/MbrCartServiceImpl.java
@@ -1,10 +1,53 @@
 package com.changfa.frame.service.mybatis.app.impl;
 
+import com.changfa.frame.mapper.app.MbrCartMapper;
 import com.changfa.frame.model.app.MbrCart;
 import com.changfa.frame.service.mybatis.app.MbrCartService;
+import com.changfa.frame.service.mybatis.common.IDUtil;
 import com.changfa.frame.service.mybatis.common.impl.BaseServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service("mbrCartServiceImpl")
 public class MbrCartServiceImpl extends BaseServiceImpl<MbrCart, Long> implements MbrCartService {
+
+    @Autowired
+    private MbrCartMapper mbrCartMapper;
+
+    /**
+     * 添加商品到购物车
+     * @param cartAdding
+     * @return
+     */
+    @Override
+    public MbrCart addProd(Long mbrId, Long wineryId, MbrCart cartAdding) {
+
+        MbrCart cart = mbrCartMapper.getByProdSkuId(cartAdding.getProdSkuId());
+
+        if (cart == null) {
+
+            MbrCart newCart = new MbrCart();
+
+            newCart.setId(IDUtil.getId());
+            newCart.setMbrId(mbrId);
+            newCart.setWineryId(wineryId);
+            newCart.setCreateDate(new Date());
+            newCart.setModifyDate(new Date());
+
+            this.save(newCart);
+
+            return newCart;
+        } else {
+
+            cart.setProdSkuCnt(cart.getProdSkuCnt() + cartAdding.getProdSkuCnt());
+            cart.setModifyDate(new Date());
+
+            this.update(cart);
+
+            return cart;
+        }
+
+    }
 }
